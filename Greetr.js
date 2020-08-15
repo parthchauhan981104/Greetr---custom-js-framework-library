eef22e4bab8fa09b5b0b74cbebf4df397c00a935
Parth <gamerboy09pc@gmail.com>
2020-08-15 13:59:43 +0530

    added methods to protoype for functionality

--- Greetr.js
+++ Greetr.js
@@ -7,8 +7,85 @@
 		// return and use a function constructor to generate a object so user no need to use 'new' keyword while using this library(like jQuery)
 	}
 
-	//Empty at start. We'll put all methods inside this that we want to be available in the object's __proto__
-	Greetr.prototype = {}; 
+	// these are never exposed anywhere, can't be accessed from the object.
+	// but can use it inside the object because of closure as the object's lexical environment is the whole outer function and it will have
+	// reference to the variables inside that function even after its execution context is over.
+	var supportedLanguages = ['en', 'es'];
+
+	var greetings = {
+		en: 'Hello',
+		es: 'Hola'
+	}
+
+	var formalGreetings = {
+		en: 'Greetings',
+		es: 'Saludos'
+	}
+
+	var logMessages = {
+		en: 'Logged in',
+		es: 'Inicio sesion'
+	}
+
+	//We'll put all methods inside this that we want to be available in the object's __proto__
+	Greetr.prototype = {
+
+		fullname: function() {
+			return this.firstName + ' ' + this.lastName;
+		},
+
+		validate: function() {
+			if (supportedLanguages.indexOf(this.language) === -1) {
+				throw "Invalid language";
+			}
+		},
+
+		greeting: function() {
+			return greetings[this.language] + ' ' + this.firstname + '!';
+		},
+
+		formalGreeting: function() {
+			return formalGreetings[this.language] + ', ' + this.fullname();
+		},
+
+		greet: function(formal) {
+            var msg;
+            
+            // if undefined or null it will be coerced to 'false'
+            if (formal) {
+                msg = this.formalGreeting();  
+            }
+            else {
+                msg = this.greeting();  
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+        
+        log: function() {
+            if (console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName()); 
+            }
+                            
+            return this;
+        },
+                            
+        setLang: function(lang) {
+            this.language = lang;
+                    
+            this.validate();
+            
+            return this;
+        }
+
+
+	}; 
 
 	Greetr.init(firstName, lastName, language){
 			var self = this;
